47a6846dfe1b4bdacc03ec9857177f3baa08b04e
Tu Nombre <luis.orihuela@upeu.edu.pe>
2025-07-25 22:51:26 -0500

    modificando la generacion de pdf para el servicio de hospedaje

--- src/main/java/com/club/control/serviceImpl/ClienteHospedajeServiceImpl.java
+++ src/main/java/com/club/control/serviceImpl/ClienteHospedajeServiceImpl.java
@@ -332,7 +332,7 @@ public class ClienteHospedajeServiceImpl implements ClienteHospedajeService {
 			// Títulos y Encabezados
 			Font titleFont = new Font(Font.HELVETICA, 20, Font.BOLD, Color.WHITE);
 			Font infoFont = new Font(Font.HELVETICA, 12, Font.NORMAL, Color.DARK_GRAY);
-			Font headerFont = new Font(Font.HELVETICA, 14, Font.BOLD, Color.WHITE);
+			Font headerFont = new Font(Font.HELVETICA, 11, Font.BOLD, Color.WHITE);
 
 			// Encabezado Mejorado
 			PdfPTable tableEncabezado = new PdfPTable(2);
@@ -354,12 +354,12 @@ public class ClienteHospedajeServiceImpl implements ClienteHospedajeService {
 			document.add(new Paragraph("RUC: 4250125244", infoFont));
 			document.add(new Paragraph("Mz L3 Lt35 Los Alamos", infoFont));
 			document.add(new Paragraph("CHACLACAYO - LIMA", infoFont));
-			document.add(new Paragraph("Fecha: " + LocalDate.now().format(DateTimeFormatter.ofPattern(DATE_FORMAT_PATTERN)),
+			document.add(new Paragraph("Fecha del reporte: " + LocalDate.now().format(DateTimeFormatter.ofPattern(DATE_FORMAT_PATTERN)),
 					infoFont));
 			document.add(Chunk.NEWLINE);
 
 			// Tabla con columnas personalizadas
-			PdfPTable table = new PdfPTable(7); // 7 columnas
+			PdfPTable table = new PdfPTable(9); // 9 columnas
 			table.setWidthPercentage(100);
 			table.setSpacingBefore(10f);
 			table.setSpacingAfter(10f);
@@ -368,42 +368,54 @@ public class ClienteHospedajeServiceImpl implements ClienteHospedajeService {
 			header1.setBackgroundColor(new Color(63, 169, 219));
 			header1.setHorizontalAlignment(Element.ALIGN_CENTER);
 			table.addCell(header1);
-
-			PdfPCell header2 = new PdfPCell(new Phrase("Cliente", headerFont));
+			
+			PdfPCell header2 = new PdfPCell(new Phrase("Precio", headerFont));
 			header2.setBackgroundColor(new Color(63, 169, 219));
 			header2.setHorizontalAlignment(Element.ALIGN_CENTER);
 			table.addCell(header2);
 
-			PdfPCell header3 = new PdfPCell(new Phrase("DNI", headerFont));
+			PdfPCell header3 = new PdfPCell(new Phrase("Cliente", headerFont));
 			header3.setBackgroundColor(new Color(63, 169, 219));
 			header3.setHorizontalAlignment(Element.ALIGN_CENTER);
 			table.addCell(header3);
 
-			PdfPCell header4 = new PdfPCell(new Phrase("Fecha Inicio", headerFont));
+			PdfPCell header4 = new PdfPCell(new Phrase("DNI", headerFont));
 			header4.setBackgroundColor(new Color(63, 169, 219));
 			header4.setHorizontalAlignment(Element.ALIGN_CENTER);
 			table.addCell(header4);
-
-			PdfPCell header5 = new PdfPCell(new Phrase("Fecha Fin", headerFont));
+			
+			PdfPCell header5 = new PdfPCell(new Phrase("Teléfono", headerFont));
 			header5.setBackgroundColor(new Color(63, 169, 219));
 			header5.setHorizontalAlignment(Element.ALIGN_CENTER);
 			table.addCell(header5);
 
-			PdfPCell header6 = new PdfPCell(new Phrase("M.Pago", headerFont));
+			PdfPCell header6 = new PdfPCell(new Phrase("Fecha Inicio", headerFont));
 			header6.setBackgroundColor(new Color(63, 169, 219));
 			header6.setHorizontalAlignment(Element.ALIGN_CENTER);
 			table.addCell(header6);
 
-			PdfPCell header7 = new PdfPCell(new Phrase("Total (S/)", headerFont));
+			PdfPCell header7 = new PdfPCell(new Phrase("Fecha Fin", headerFont));
 			header7.setBackgroundColor(new Color(63, 169, 219));
 			header7.setHorizontalAlignment(Element.ALIGN_CENTER);
 			table.addCell(header7);
 
+			PdfPCell header8 = new PdfPCell(new Phrase("M.Pago", headerFont));
+			header8.setBackgroundColor(new Color(63, 169, 219));
+			header8.setHorizontalAlignment(Element.ALIGN_CENTER);
+			table.addCell(header8);
+
+			PdfPCell header9 = new PdfPCell(new Phrase("Total (S/)", headerFont));
+			header9.setBackgroundColor(new Color(63, 169, 219));
+			header9.setHorizontalAlignment(Element.ALIGN_CENTER);
+			table.addCell(header9);
+
 			// Datos
 			for (ClienteHospedajeEntity entity : datos) {
 				table.addCell(entity.getHospedaje().getCodigoHabitacion());
+				table.addCell(String.valueOf(entity.getHospedaje().getPrecio()));
 				table.addCell(entity.getCliente().getName() + " " + entity.getCliente().getLastName());
 				table.addCell(entity.getCliente().getDni());
+				table.addCell(entity.getCliente().getTelephone());
 				table.addCell(entity.getFechaInicio().toString());
 				table.addCell(entity.getFechaFin().toString());
 				table.addCell(entity.getMetodoPago().getName());
@@ -444,7 +456,7 @@ public class ClienteHospedajeServiceImpl implements ClienteHospedajeService {
 			// Títulos y Encabezados
 			Font titleFont = new Font(Font.HELVETICA, 20, Font.BOLD, Color.WHITE);
 			Font infoFont = new Font(Font.HELVETICA, 12, Font.NORMAL, Color.DARK_GRAY);
-			Font headerFont = new Font(Font.HELVETICA, 14, Font.BOLD, Color.WHITE);
+			Font headerFont = new Font(Font.HELVETICA, 11, Font.BOLD, Color.WHITE);
 
 			// Encabezado Mejorado
 			PdfPTable tableEncabezado = new PdfPTable(2);
@@ -472,7 +484,7 @@ public class ClienteHospedajeServiceImpl implements ClienteHospedajeService {
 			document.add(Chunk.NEWLINE);
 
 			// Tabla con columnas personalizadas
-			PdfPTable table = new PdfPTable(7); // 7 columnas
+			PdfPTable table = new PdfPTable(9);
 			table.setWidthPercentage(100);
 			table.setSpacingBefore(10f);
 			table.setSpacingAfter(10f);
@@ -481,41 +493,53 @@ public class ClienteHospedajeServiceImpl implements ClienteHospedajeService {
 			header1.setBackgroundColor(new Color(63, 169, 219));
 			header1.setHorizontalAlignment(Element.ALIGN_CENTER);
 			table.addCell(header1);
-
-			PdfPCell header2 = new PdfPCell(new Phrase("Cliente", headerFont));
+			
+			PdfPCell header2 = new PdfPCell(new Phrase("Precio", headerFont));
 			header2.setBackgroundColor(new Color(63, 169, 219));
 			header2.setHorizontalAlignment(Element.ALIGN_CENTER);
 			table.addCell(header2);
 
-			PdfPCell header3 = new PdfPCell(new Phrase("DNI", headerFont));
+			PdfPCell header3 = new PdfPCell(new Phrase("Cliente", headerFont));
 			header3.setBackgroundColor(new Color(63, 169, 219));
 			header3.setHorizontalAlignment(Element.ALIGN_CENTER);
 			table.addCell(header3);
 
-			PdfPCell header4 = new PdfPCell(new Phrase("Fecha Ingreso", headerFont));
+			PdfPCell header4 = new PdfPCell(new Phrase("DNI", headerFont));
 			header4.setBackgroundColor(new Color(63, 169, 219));
 			header4.setHorizontalAlignment(Element.ALIGN_CENTER);
 			table.addCell(header4);
-
-			PdfPCell header5 = new PdfPCell(new Phrase("Fecha Salida", headerFont));
+			
+			PdfPCell header5 = new PdfPCell(new Phrase("Teléfono", headerFont));
 			header5.setBackgroundColor(new Color(63, 169, 219));
 			header5.setHorizontalAlignment(Element.ALIGN_CENTER);
 			table.addCell(header5);
 
-			PdfPCell header6 = new PdfPCell(new Phrase("M.Pago", headerFont));
+			PdfPCell header6 = new PdfPCell(new Phrase("Fecha Inicio", headerFont));
 			header6.setBackgroundColor(new Color(63, 169, 219));
 			header6.setHorizontalAlignment(Element.ALIGN_CENTER);
 			table.addCell(header6);
 
-			PdfPCell header7 = new PdfPCell(new Phrase("Total (S/)", headerFont));
+			PdfPCell header7 = new PdfPCell(new Phrase("Fecha Fin", headerFont));
 			header7.setBackgroundColor(new Color(63, 169, 219));
 			header7.setHorizontalAlignment(Element.ALIGN_CENTER);
 			table.addCell(header7);
 
+			PdfPCell header8 = new PdfPCell(new Phrase("M.Pago", headerFont));
+			header8.setBackgroundColor(new Color(63, 169, 219));
+			header8.setHorizontalAlignment(Element.ALIGN_CENTER);
+			table.addCell(header8);
+
+			PdfPCell header9 = new PdfPCell(new Phrase("Total (S/)", headerFont));
+			header9.setBackgroundColor(new Color(63, 169, 219));
+			header9.setHorizontalAlignment(Element.ALIGN_CENTER);
+			table.addCell(header9);
+
 			// Datos
 			table.addCell(entity.getHospedaje().getCodigoHabitacion());
+			table.addCell(String.valueOf(entity.getHospedaje().getPrecio()));
 			table.addCell(entity.getCliente().getName() + " " + entity.getCliente().getLastName());
 			table.addCell(entity.getCliente().getDni());
+			table.addCell(entity.getCliente().getTelephone());
 			table.addCell(entity.getFechaInicio().toString());
 			table.addCell(entity.getFechaFin().toString());
 			table.addCell(entity.getMetodoPago().getName());
